f369f6edec6908d20547167c984c3810fff7a243
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-06-30 15:28:56 +0900

    Update chenge.js

--- chenge.js
+++ chenge.js
@@ -3,18 +3,26 @@ function dateChengeToNext() {
     let times = data[0].timeSeries[0].timeDefines
     let number = times.length
     if(Number(document.getElementById("next").dataset.now)+1 <= number-1){
+      document.getElementById("back").dataset.now = "off"
+      if(Number(document.getElementById("next").dataset.now)+1 == number-1){
+        document.getElementById("next").dataset.now = "on"
+      }
       mapDrawAll(Number(document.getElementById("next").dataset.now)+1)
-      document.getElementById("next").dataset.now = Number(document.getElementById("next").dataset.now)+1
       document.getElementById("back").dataset.now = Number(document.getElementById("next").dataset.now)+1
+      document.getElementById("next").dataset.now = Number(document.getElementById("next").dataset.now)+1
     }
   })
 }
 function dateChengeToBack() {
   $.getJSON("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json", function (data) {
-    if(Number(document.getElementById("back").dataset.now)-1 > 0){
+    if(Number(document.getElementById("back").dataset.now)-1 >= 0){
+      document.getElementById("next").dataset.now = "off"
+      if(Number(document.getElementById("back").dataset.now)-1 == 0){
+        document.getElementById("back").dataset.now = "on"
+      }
       mapDrawAll(Number(document.getElementById("back").dataset.now)-1)
-      document.getElementById("back").dataset.now = Number(document.getElementById("back").dataset.now)-1
       document.getElementById("next").dataset.now = Number(document.getElementById("back").dataset.now)-1
+      document.getElementById("back").dataset.now = Number(document.getElementById("back").dataset.now)-1
     }
   })
 }
