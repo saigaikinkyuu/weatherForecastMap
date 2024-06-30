fe3d2cc13673d347dbec15af993a0d36faf5fd23
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-06-30 14:14:52 +0900

    Update chenge.js

--- chenge.js
+++ chenge.js
@@ -3,7 +3,7 @@ function dateChengeToNext() {
     let times = data[0].timeSeries[0].timeDefines
     let number = times.length
     if(Number(document.getElementById("next").dataset.now)+1 <= number-1){
-      drawMapAll(Number(document.getElementById("next").dataset.now)+1)
+      mapDrawAll(Number(document.getElementById("next").dataset.now)+1)
       document.getElementById("next").dataset.now = Number(document.getElementById("next").dataset.now)+1
       document.getElementById("back").dataset.now = Number(document.getElementById("next").dataset.now)+1
     }
@@ -12,7 +12,7 @@ function dateChengeToNext() {
 function dateChengeToBack() {
   $.getJSON("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json", function (data) {
     if(Number(document.getElementById("back").dataset.now)-1 >= 0){
-      drawMapAll(Number(document.getElementById("back").dataset.now)-1)
+      mapDrawAll(Number(document.getElementById("back").dataset.now)-1)
       document.getElementById("back").dataset.now = Number(document.getElementById("back").dataset.now)-1
       document.getElementById("next").dataset.now = Number(document.getElementById("back").dataset.now)-1
     }
