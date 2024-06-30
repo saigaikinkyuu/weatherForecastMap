326f254ce815ab666fd112bca4a85f86b9d30616
saigaikinkyuu <125773463+saigaikinkyuu@users.noreply.github.com>
2024-06-30 13:45:18 +0900

    Create chenge.js

---
+++ chenge.js
@@ -0,0 +1,18 @@
+function dateChengeToNext() {
+  $.getJSON("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json", function (data) {
+    let times = data[0].timeSeries[0].timeDefines
+    let number = times.length
+    if(Number(document.getElementById("next").datest.now)+1 <= number-1){
+      drawMapAll(Number(document.getElementById("next").datest.now)+1)
+      document.getElementById("next").datest.now = Number(document.getElementById("next").datest.now)+1
+    }
+  })
+}
+function dateChengeToBack() {
+  $.getJSON("https://www.jma.go.jp/bosai/forecast/data/forecast/130000.json", function (data) {
+    if(Number(document.getElementById("back").datest.now)-1 >= 0){
+      drawMapAll(Number(document.getElementById("back").datest.now)-1)
+      document.getElementById("back").datest.now = Number(document.getElementById("back").datest.now)-1
+    }
+  })
+}
